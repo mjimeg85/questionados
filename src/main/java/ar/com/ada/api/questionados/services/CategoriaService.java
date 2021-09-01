3dee4a51bbd9c1814b8bee65328705a28d7c2ae8
mjimeg85 <mjimeg85@gmail.com>
2021-09-01 23:06:03 +0200

    Se crean 2 metodos en los services de Categoria y Pregunta.

--- src/main/java/ar/com/ada/api/questionados/services/CategoriaService.java
+++ src/main/java/ar/com/ada/api/questionados/services/CategoriaService.java
@@ -1,5 +1,33 @@
 package ar.com.ada.api.questionados.services;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.com.ada.api.questionados.entities.Categoria;
+import ar.com.ada.api.questionados.repos.CategoriaRepository;
+
+@Service
 public class CategoriaService {
     
+    @Autowired
+    CategoriaRepository repo;
+
+    public List<Categoria> traerCategorias(){
+        return repo.findAll();
+    }
+
+    public Categoria buscarCategoriaPorId(Integer categoriaId) {
+
+        Optional<Categoria> resultado = repo.findById(categoriaId);
+        Categoria categoria = null;
+
+        if (resultado.isPresent())
+            categoria = resultado.get();
+
+        return categoria;
+
+    }
+
 }
--- src/main/java/ar/com/ada/api/questionados/services/PreguntaService.java
+++ src/main/java/ar/com/ada/api/questionados/services/PreguntaService.java
@@ -1,5 +1,32 @@
 package ar.com.ada.api.questionados.services;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.com.ada.api.questionados.entities.Pregunta;
+import ar.com.ada.api.questionados.repos.PreguntaRepository;
+
+@Service
 public class PreguntaService {
+
+    @Autowired
+    PreguntaRepository repo;
+
+    public List<Pregunta> traerPreguntas(){
+        return repo.findAll();
+    }
+
+    public Pregunta buscarPreguntaPorId(Integer preguntaId) {
+
+        Optional<Pregunta> resultado = repo.findById(preguntaId);
+
+        if (resultado.isPresent())
+            return resultado.get();
+
+        return null;
+    }
+
     
 }
