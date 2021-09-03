5ef30cc469cfce879f17e5074abf05ad2fe6b989
mjimeg85 <mjimeg85@gmail.com>
2021-09-03 22:56:56 +0200

    Se crean endpoints GET en CategoriaController y PreguntaController

--- src/main/java/ar/com/ada/api/questionados/controllers/CategoriaController.java
+++ src/main/java/ar/com/ada/api/questionados/controllers/CategoriaController.java
@@ -1,8 +1,32 @@
 package ar.com.ada.api.questionados.controllers;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.ada.api.questionados.entities.Categoria;
+import ar.com.ada.api.questionados.services.CategoriaService;
 
 @RestController
 public class CategoriaController {
+    
+    @Autowired
+    CategoriaService service;
+
+    @GetMapping("/categoria")
+    public ResponseEntity<List<Categoria>> traerCategorias() { //return Response Entity
+        return ResponseEntity.ok(service.traerCategorias()); //return entity con el valor esperado
+    }
+
+    @GetMapping("/categoria/{id}")
+    public ResponseEntity<Categoria> buscarCategoriaPorId(@PathVariable Integer categoriaId) { 
+        return ResponseEntity.ok(service.buscarCategoriaPorId(categoriaId)); 
+    }
+
+
     
 }
--- src/main/java/ar/com/ada/api/questionados/controllers/PreguntaController.java
+++ src/main/java/ar/com/ada/api/questionados/controllers/PreguntaController.java
@@ -1,8 +1,29 @@
 package ar.com.ada.api.questionados.controllers;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.ada.api.questionados.entities.Pregunta;
+import ar.com.ada.api.questionados.services.PreguntaService;
 
 @RestController
 public class PreguntaController {
+
+    @Autowired
+    PreguntaService service;
+
+    @GetMapping("/pregunta")
+    public ResponseEntity<List<Pregunta>> traerPreguntas() { //return Response Entity
+        return ResponseEntity.ok(service.traerPreguntas()); //return entity con el valor esperado
+    }
     
+    @GetMapping("/pregunta/{id}")
+    public ResponseEntity<Pregunta> buscarPreguntaPorId(@PathVariable Integer preguntaId) { 
+        return ResponseEntity.ok(service.buscarPreguntaPorId(preguntaId)); 
+    }
 }
